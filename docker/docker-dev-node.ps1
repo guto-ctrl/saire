c2b972da18899c61ec5f7e34a8ed645f294edbb8
guto401 <87216391+guto401@users.noreply.github.com>
2026-04-18 15:19:27 -0300

    Create docker-dev-node.ps1

---
+++ docker/docker-dev-node.ps1
@@ -0,0 +1,14 @@
+# Remove o container antigo se existir (porque o Docker não esquece, ele só julga)
+docker rm -f g-node 2>$null
+
+# Cria e roda o container em background
+docker run -dit `
+  --name g-node `
+  -v "${PWD}:/app" `
+  -w /app `
+  -p 3000:3000 `
+  node:20-alpine `
+  sh
+
+# Entra no container
+docker exec -it g-node sh
