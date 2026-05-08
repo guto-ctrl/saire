6a08aa8dd31d0bd1c487e484f0debe4e9db5080a
guto401 <87216391+guto401@users.noreply.github.com>
2026-05-08 18:03:16 -0300

    Create Compressor.cs

---
+++ backend/Domain/Compressor.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Domain
+{
+    public class Compressor
+    {
+
+        public int CompressorId { get; set; }
+
+        [Required]
+        [StringLength(30)]
+        public string Modelo { get; set; } = String.Empty;
+
+        [Required]
+        [StringLength(15)]
+        public string Marca { get; set; } = String.Empty;
+
+        public int? VoltagemNacional { get; set; }
+
+        public int? Frequencia { get; set; }
+
+        public decimal? Corrente { get; set; }
+
+        public decimal? CorrenteMotorY { get; set; }
+
+        public decimal? CorrenteMotorYY { get; set; }
+
+        public decimal? VolumeDeslocamento { get; set; }
+
+        public int? Rotacao { get; set; }
+
+        public DateTime DataCadastro { get; set; } = DateTime.UtcNow;
+
+    }
+}
