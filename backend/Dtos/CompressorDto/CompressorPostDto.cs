74b25d64c0a92e43f1793e1e9824a2466e9ecb1b
guto401 <87216391+guto401@users.noreply.github.com>
2026-05-08 18:03:20 -0300

    Create CompressorPostDto.cs

---
+++ backend/Dtos/CompressorDto/CompressorPostDto.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dtos.CompressorDto
+{
+    public class CompressorPostDto
+    {
+        [Required]
+        [StringLength(30)]
+        public string Modelo { get; set; } = String.Empty;
+
+        [Required]
+        [StringLength(15)]
+        public string Marca { get; set; } = String.Empty;
+
+        public int? VoltagemNacional { get; set; }
+
+        public int? Frequencia { get; set; }
+
+        public decimal? Corrente { get; set; }
+
+        public decimal? CorrenteMotorY { get; set; }
+
+        public decimal? CorrenteMotorYY { get; set; }
+
+        public decimal? VolumeDeslocamento { get; set; }
+
+        public int? Rotacao { get; set; }
+    }
+}
