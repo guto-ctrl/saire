37408aef8709db5dea4239a1f2b9d81ddb3262fc
guto401 <87216391+guto401@users.noreply.github.com>
2026-05-08 18:03:18 -0300

    Create CompressorPatchDto.cs

---
+++ backend/Dtos/CompressorDto/CompressorPatchDto.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace backend.Dtos.CompressorDto
+{
+    public class CompressorPatchDto
+    {
+        [StringLength(30)]
+        public string? Modelo { get; set; }
+
+        [StringLength(15)]
+        public string? Marca { get; set; }
+
+        public int? VoltagemNacional { get; set; }
+
+        public int? Frequencia { get; set; }
+
+        public decimal? Corrente { get; set; }
+
+        public decimal? CorrenteMotorY { get; set; }
+
+        public decimal? CorrenteMotorYY { get; set; }
+
+        public decimal? VolumeDeslocamento { get; set; }
+
+        public int? Rotacao { get; set; }
+    }
+}
