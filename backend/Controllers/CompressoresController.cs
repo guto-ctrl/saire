bd5f04d1cf37d8a7bd6f864a8adc392909f3ffee
guto401 <87216391+guto401@users.noreply.github.com>
2026-05-08 18:03:10 -0300

    Create CompressoresController.cs

---
+++ backend/Controllers/CompressoresController.cs
@@ -0,0 +1,67 @@
+﻿using backend.Dtos.CompressorDto;
+using backend.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class CompressoresController : ControllerBase
+    {
+        // Injetando contexto pra instância service
+        private readonly CompressorService _service;
+        public CompressoresController(CompressorService service)
+        {
+            _service = service;
+        }
+
+        // Método GetAll
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var compressores = await _service.GetAllAsync();
+            return Ok(compressores);
+        }
+
+        // Método GetItem
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetItem(int id)
+        {
+            var compressor = await _service.GetItemAsync(id);
+            if (compressor == null)
+            {
+                return NotFound();
+            }
+            return Ok(compressor);
+        }
+
+        // Método Post
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] CompressorPostDto dto)
+        {
+
+            var newCompressor = await _service.CreateAsync(dto);
+
+            return CreatedAtAction(
+                nameof(GetItem),
+                new { id = newCompressor.CompressorId },
+                newCompressor
+            );
+            //return Ok(dto);
+        }
+
+        // Método Patch
+        [HttpPatch]
+        public async Task<IActionResult> Patch(int id, [FromBody] CompressorPatchDto dto)
+        {
+            var compressor = await _service.UpdateAsync(id, dto);
+
+            if (compressor == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(compressor);
+        }
+    }
+}
