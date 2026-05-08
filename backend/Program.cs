d18a93ce5a976faf34aefe09192f64263c0c2687
guto401 <87216391+guto401@users.noreply.github.com>
2026-05-08 18:03:22 -0300

    Create Program.cs

---
+++ backend/Program.cs
@@ -0,0 +1,33 @@
+using backend.Data.Context;
+using backend.Services;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adicionando serviços do container
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddOpenApi();
+builder.Services.AddSwaggerGen();
+builder.Services.AddScoped<CompressorService>(); // Service responsável pelos compressores
+
+// Configuração de conexão com a database
+string SQLiteConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDbContext>(
+    options =>
+        options.UseSqlite(SQLiteConnection)
+);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    //app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+app.MapControllers();
+
+app.Run();
